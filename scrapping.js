75f5929bf50544a6a4809cf32cbd876233979dbf
Jose Lorenzo Moreno <jolorenzom@gmail.com>
2020-01-29 00:27:22 +0100

    Quit sensitive data.

--- scrapping.js
+++ scrapping.js
@@ -3,13 +3,12 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 
 let slackConfig = {
-	webhook_url: 'https://hooks.slack.com/services/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' // #feed-bots
-	// 	webhook_url: 'https://hooks.slack.com/services/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' // #tests
+	webhook_url: 'https://hooks.slack.com/services/XXXXX/XXXXXX/XXXXXX'
 	,botName: 'Robin'
 };
 
 const slack = new Slack(slackConfig.webhook_url);
-const message = `Hola Kubide, esto es un mensaje desde un bot de Node`;
+const message = `Hola, esto es un mensaje desde un bot de Node`;
 const url2 = 'https://es.wikipedia.org/wiki/Wikipedia:Portada';
 
 rp(url2)
